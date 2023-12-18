4ca9bc0d571032b6b817d7b0240242b8aaaf8c0d
AbogeJr <junioraboge@gmail.com>
2023-12-18 13:11:28 +0300

    update: comleted auth routes and added order routes

--- database.py
+++ database.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine('postgresql://postgres:root@localhost/chapchap',
-    echo = True
-)
+engine = create_engine("postgresql://aboge:root@localhost/chapchap", echo=True)
 
 Base = declarative_base()
 
 Session = sessionmaker()
-
